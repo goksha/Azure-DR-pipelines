05a113ad90e0b52905bbc2afd19f4bd29d9793ed
goksha <shashigokhale@gmail.com>
2023-07-30 18:08:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,12 +34,12 @@ resource "azurerm_kubernetes_cluster" "aks_cluster_primary" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.aks_cluster_primary.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  value = azurerm_kubernetes_cluster.aks_cluster_primary.kube_config_raw
 
   sensitive = true
 }
